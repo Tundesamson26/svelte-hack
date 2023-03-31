36ca2b0b604b288aabb288166ad5a2f63d81726f
Tunde Samson <odewoletundesamson@gmail.com>
2023-03-31 22:00:25 +0100

    customize tailwind csss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,16 @@
+const defaultTheme = require(tailwindcss/defaultTheme);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    colors:{},
+    extend: {
+      fontFamily:{
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'),],
 }
 
