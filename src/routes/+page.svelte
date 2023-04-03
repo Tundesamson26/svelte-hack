5342abd135bdc075fe962e6d804a5f8bf94e4ee9
Tunde Samson <odewoletundesamson@gmail.com>
2023-04-03 17:53:21 +0100

    image generator styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-  import  imageStores from "$lib/store/ArtStore";
+  import imageStores from "$lib/store/ArtStore";
   import { Configuration, OpenAIApi } from "openai";
 
   let prompt;
-  let image = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-e4LTVTBp1C2nhyRwB1ZHhKL8/user-fVk8kO2uHbVysumf0AfU26o2/img-rkDsUfv400QxPQn8KKvLSjNi.png?st=2023-04-01T20%3A30%3A51Z&se=2023-04-01T22%3A30%3A51Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-04-01T18%3A15%3A39Z&ske=2023-04-02T18%3A15%3A39Z&sks=b&skv=2021-08-06&sig=%2BXIyjN71FeTUQxOGimiSGerSL4Jos5THjuo/EbbbRj8%3D";
+  let image = "";
 
   const configuration = new Configuration({
     organization: "org-e4LTVTBp1C2nhyRwB1ZHhKL8",
@@ -18,20 +18,32 @@
         n: 2,
         size: "512x512",
       });
-      console.log(response.data.data[0].url)
+      console.log(response.data.data[0].url);
     } catch (err) {
       console.log(err);
     }
   };
 </script>
 
-<h1 class="text-3xl font-bold" on:submit={fetchData}>Image Generator</h1>
-<form action="">
-  <input bind:value={image} type="text" placeholder="enter image description here...">
-  <button type="submit" on:click={fetchData}>Generate</button>
-</form>
-<img src={image} alt="">
-
+<h1 class="text-3xl font-bold text-[#57534e] text-center my-7">
+  Image Generator
+</h1>
+<div>
+  <form action="" >
+    <div class="flex justify-between w-full">
+      <input
+        bind:value={image}
+        type="text"
+        placeholder="enter image description here..."
+        class="block rounded-md w-screen border-0 py-1.5 ml-4 font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      />
+      <button type="submit" on:click={fetchData} class="rounded-md text-[#57534e] font-semibold py-2 px-8 ml-5 mr-8 bg-orange-500">Generate</button>
+    </div>
+  </form>
+</div>
+<div>
+  <img src={image} alt="" />
+</div>
 
 <style lang="postcss">
   :global(html) {
