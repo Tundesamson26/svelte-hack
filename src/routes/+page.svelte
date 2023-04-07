d8112dc3c6d8a384849487d218622d470442e16c
Tunde Samson <odewoletundesamson@gmail.com>
2023-04-07 23:21:49 +0100

    homepage update commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,6 +33,7 @@
       <input
         type="text"
         placeholder="enter image description here..."
+        on:change={(e)=> setPrompt(e.target.value)}
         class="block rounded-md w-screen border-0 py-1.5 ml-4 font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
       />
       <button type="submit" on:click={fetchData} class="rounded-md text-[#57534e] font-semibold py-2 px-8 ml-5 mr-8 bg-orange-500">Generate</button>
