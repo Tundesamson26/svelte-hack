3c1d521bdc3daca3251b296407487ec78d0b0bf7
Tunde Samson <odewoletundesamson@gmail.com>
2023-04-10 22:20:15 +0100

    updated commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import { Configuration, OpenAIApi } from "openai";
 
   let prompt;
+  let image = ""
   
 
   const configuration = new Configuration({
@@ -28,24 +29,25 @@
   Image Generator
 </h1>
 <div>
-  <form action="" >
     <div class="flex justify-between w-full">
       <input
         type="text"
         placeholder="enter image description here..."
-        on:change={(e)=> setPrompt(e.target.value)}
         class="block rounded-md w-screen border-0 py-1.5 ml-4 font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
       />
       <button type="submit" on:click={fetchData} class="rounded-md text-[#57534e] font-semibold py-2 px-8 ml-5 mr-8 bg-orange-500">Generate</button>
     </div>
-  </form>
 </div>
-<div>
-  <img src="" alt="" />
+<div class="img-container">
+  <img src={image} alt="" />
 </div>
 
 <style lang="postcss">
   :global(html) {
     background-color: theme(colors.gray.100);
+  }
+  :img-container{
+    width: 512;
+    height: 512;
   }
 </style>
